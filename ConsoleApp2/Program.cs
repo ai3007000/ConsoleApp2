7af10dd457e4ccf562d848448ba7164339166dba
Артём Иванов <ai5007000@yandex.ru>
2023-12-30 21:33:10 +0700

    Задание 12.1.5 выполнено

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,8 +6,37 @@ namespace ConsoleApp2.ConsoleApp
     {
         static void Main(string[] args)
         {
-
+            User[] people = new User[]
+            {
+                new User("euw3uif", "Artem", true),
+                new User("wiobnfw", "Sofy", true),
+                new User("wfne", "Liara", false)
+            };
+            foreach (var item in people)
+            {
+                Console.WriteLine($"Здравствуй, {item.Name}!");
+                if (!item.IsPremium)
+                {
+                    ShowAds();
+                }
+            }
         }
+        /// <summary>
+        /// Реклама
+        /// </summary>
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
 
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
     }
 }
---
+++ ConsoleApp2/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        public User(string Login, string Name, bool IsPremium)
+        {
+            this.Login = Login;
+            this.Name = Name;
+            this.IsPremium = IsPremium;
+        }
+    }
+}
